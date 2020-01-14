bf54a80119564b1cf6b850e10903d25aeaf39f31
Gabriele Besta <48332305+bg-master@users.noreply.github.com>
2020-01-14 21:35:37 +0100

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,11 @@ FROM alpine
 
 RUN apk add --no-cache tini iptables
 
-ADD configure-firewall.sh /bin
-
-ENV FIREWALL_CHAIN "DOCKER-FIREWALL"
-ENV FIREWALL_OPEN_TCP_PORTS "22"
-ENV FIREWALL_OPEN_UDP_PORTS ""
-ENV FIREWALL_ACCEPT_ALL_FROM ""
+COPY /script/* /script/
+RUN mv /script/configure-firewall.sh /usr/local/bin && \
+    chmod +x /usr/local/bin/configure-firewall.sh && \
+    ln -s /usr/local/bin/configure-firewall.sh /
 
 ENTRYPOINT ["/sbin/tini", "--"]
 
-CMD ["/bin/configure-firewall.sh"]
+CMD ["configure-firewall.sh"]
--- script/configure-firewall.sh
+++ script/configure-firewall.sh
@@ -30,27 +30,27 @@ fi
 iptables -t mangle -F $FIREWALL_CHAIN
 
 # Drop invalid packets
-iptables -t mangle -A $FIREWALL_CHAIN -m conntrack --ctstate INVALID -j DROP 
+iptables -t mangle -A $FIREWALL_CHAIN -m conntrack --ctstate INVALID -j DROP
 # Drop TCP packets that are new and are not SYN
 iptables -t mangle -A $FIREWALL_CHAIN -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 # Drop SYN packets with suspicious MSS value
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP  
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 # Block packets with bogus TCP flags
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,ACK FIN -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,URG URG -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,FIN FIN -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,PSH PSH -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL ALL -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL NONE -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
-iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,URG URG -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,FIN FIN -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ACK,PSH PSH -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL ALL -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL NONE -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
 iptables -t mangle -A $FIREWALL_CHAIN -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 # Drop ICMP
-iptables -t mangle -A $FIREWALL_CHAIN -p icmp -j DROP 
+iptables -t mangle -A $FIREWALL_CHAIN -p icmp -j DROP
 # Drop fragments in all chains
 iptables -t mangle -A $FIREWALL_CHAIN -f -j DROP
 
